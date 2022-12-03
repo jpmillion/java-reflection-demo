5aa3d26eb9c072737d91c4b50843345bfee7ccfb
John Million <john.million@rtslabs.com>
2022-12-03 14:27:14 -0500

    Define findAllImplementedInterfaces in ClassAnalyzer

--- src/main/java/com/million/exercises/ClassAnalyzer.java
+++ src/main/java/com/million/exercises/ClassAnalyzer.java
@@ -50,4 +50,21 @@ public class ClassAnalyzer {
         }
         return inheritedClasses;
     }
+
+    /**
+     * Returns all the interfaces that the current input class implements.
+     * Note: If the input is an interface itself, the method returns all the interfaces the
+     * input interface extends.
+     */
+    public static Set<Class<?>> findAllImplementedInterfaces(Class<?> input) {
+        Set<Class<?>> allImplementedInterfaces = new HashSet<>();
+
+        Class<?>[] inputInterfaces = input.getInterfaces();
+        for (Class<?> currentInterface : inputInterfaces) {
+            allImplementedInterfaces.add(currentInterface);
+            allImplementedInterfaces.addAll(findAllImplementedInterfaces(currentInterface));
+        }
+
+        return allImplementedInterfaces;
+    }
 }
